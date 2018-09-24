c22cfbfafd0af1b81f8aafd353666e619a99d73c
Palina Kutsilina <palina.kutsilina@gmail.com>
2018-09-24 20:09:24 +0300

    Love triangles fix

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let processedSet = new Set();
+    let result = 0;
+    for (let i = 0; i + 2 <= preferences.length; i++) {
+        let firstNormalValue = preferences[i] - 1;
+        if (processedSet.has(firstNormalValue)) {
+            //already processed
+            continue;
+        }
+        if (firstNormalValue == i) {
+            //self lover!!
+            continue;
+        }
+        let secondNormalValue = preferences[firstNormalValue] - 1;
+        let thirdNormalValue = preferences[secondNormalValue] - 1;
+        if (thirdNormalValue == i){
+            result += 1;
+            processedSet.add(firstNormalValue);
+            processedSet.add(secondNormalValue);
+            processedSet.add(thirdNormalValue);
+        }
+    }
+    return result;
 };
